969b72b993197b949578b54a566dfa4703feec88
R. S. Doiel <rsdoiel@gmail>
2026-05-05 15:29:53 -0700

    Quick Save

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# generated with CMTools 0.0.3 3a81bb4
+# generated with CMTools 0.0.3 cf7aad3
 
 #
 # Set the package name and version to install
--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2026-05-05"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "3a81bb4"
+    ReleaseHash = "cf7aad3"
     LicenseText = `
 audioinfo a Go module for managing audio metadata in a SQLite3 database
 Copyright (C) 2026 R. S. Doiel
